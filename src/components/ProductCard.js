469361068c48dc1913b332fa11d36a034731bf4a
amaresh329 <vutukuriamaresh930@gmail.com>
2025-05-19 21:07:22 +0530

    adding the reducers

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,22 @@
 import "./ProductCard.css";
+import { add, remove } from "../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
+export const ProductCard = ({ product }) => {
+  const { id, name, price, image } = product;
+  const dispatch = useDispatch();
+  const cartList = useSelector((state) => state.cartState.cartList);
+  const [isInCart, setIsInCart] = useState(false);
 
-export const ProductCard = ({product}) => {
-  const {name, price, image} = product;
+  useEffect(() => {
+    const productInCart = cartList.find((item) => item.id === id);
+    if(productInCart){
+      setIsInCart(true);
+    }else{
+      setIsInCart(false);
+    }
+  }, [cartList,id]);
 
   return (
     <div className="productCard">
@@ -9,8 +24,8 @@ export const ProductCard = ({product}) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button>Add To Cart</button>
+        {isInCart?<button className="remove" onClick={() => dispatch(remove(product))}>Remove</button>:(<button onClick={() => dispatch(add(product))}>Add To Cart</button>)}
       </div>
     </div>
-  )
-}
+  );
+};
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -8,14 +8,18 @@ const cartSlice=createSlice({
     },
     reducers:{
         add(state,action){
-            cartList:[],
-             total:0  
+           const updatedCartList=state.cartList.concat(action.payload);
+           const total=state.total+action.payload.price;
+           return {...state,total:total,cartList:updatedCartList};
+           
         },
-        remove(){
-
+        remove(state,action){
+        const updatedCartList=state.cartList.filter(item=>item.id!== action.payload.id);
+        const total=state.total-action.payload.price;
+        return {...state,total:total,cartList:updatedCartList};
         }
     }
 });
 
 export const {add,remove} =cartSlice.actions;
-export const cartReducer=createSlice.reducer; 
+export const cartReducer=cartSlice.reducer; 
